fixed Rfree calculation
Former-commit-id: 40ef2b80ef49d84647cafb1d6f3cbb3a310eec11
Former-commit-id: e964f4c796e6b5b9acf329fd3465210b00e7a98e

GENFIRE/fileio.py
@@ -352,8 +352,9 @@ def saveResults(reconstruction_outputs, filename):
     fn, ext = os.path.splitext(filename)
     writeVolume(filename, reconstruction_outputs['reconstruction'])
     np.savetxt(fn+'_errK.txt',reconstruction_outputs['errK'])
-    if 'R_free' in reconstruction_outputs.keys():
-        np.savetxt(fn+'_Rfree.txt',reconstruction_outputs['R_free'])
+    if 'R_free_total' in reconstruction_outputs.keys():
+        np.savetxt(fn+'_Rfree_total.txt',reconstruction_outputs['R_free_total'])
+        np.savetxt(fn+'_Rfree_bybin.txt',reconstruction_outputs['R_free_bybin'])
 
 def writeVolume(filename, data, order="C"):
     """

GENFIRE/gui/launch.py
@@ -339,27 +339,28 @@ class GenfireMainWindow(QtGui.QMainWindow):
                 plt.xlabel("Iteration Num")
                 plt.ylabel("Reciprocal Error")
                 # plt.setp(plt.gcf(),)
-            Rfree_filename = output_base + '_Rfree.txt'
-
-            if os.path.isfile(Rfree_filename):
-                Rfree = np.loadtxt(Rfree_filename)
-                numIterations = np.shape(Rfree)[1]
+            Rfree_total_filename = output_base + '_Rfree_total.txt'
+            Rfree_bybin_filename = output_base + '_Rfree_bybin.txt'
+            if os.path.isfile(Rfree_total_filename):
+                Rfree_total = np.loadtxt(Rfree_total_filename)
+                numIterations = np.shape(Rfree_total)[0]
                 plt.figure()
                 plt.title("R-free vs Iteration Number")
                 mngr = plt.get_current_fig_manager()
                 mngr.window.setGeometry(700,350,550, 250)
-                plt.plot(range(0,numIterations),np.mean(Rfree,axis=0))
+                plt.plot(range(0,numIterations),Rfree_total)
                 plt.title("Mean R-free Value vs Iteration Number")
                 plt.xlabel("Iteration Num")
                 plt.ylabel('Mean R-free')
 
-
+            if os.path.isfile(Rfree_bybin_filename):
+                Rfree_bybin = np.loadtxt(Rfree_bybin_filename)
                 plt.figure()
                 mngr = plt.get_current_fig_manager()
                 mngr.window.setGeometry(700,650,550, 250)
                 plt.hold(False)
-                X = np.linspace(0,1,np.shape(Rfree)[0])
-                plt.plot(X, Rfree[:,-1])
+                X = np.linspace(0,1,np.shape(Rfree_bybin)[0])
+                plt.plot(X, Rfree_bybin[:,-1])
                 plt.hold(False)
                 plt.title("Final Rfree Value vs Spatial Frequency")
                 plt.xlabel("Spatial Frequency (% of Nyquist)")

GENFIRE/main.py
@@ -96,7 +96,7 @@ def main(reconstruction_parameters):
     if useDefaultSupport or filename_support == "":
         support = np.ones((dims[0],dims[0],dims[0]),dtype=float)
     else:
-        support = GENFIRE.fileio.readVolume(filename_support)
+        support = (GENFIRE.fileio.readVolume(filename_support) != 0).astype(bool)
 
     displayFigure.reconstructionDisplayWindowSize = np.shape(support) # this is used to show the central region of reconstruction
 
@@ -134,7 +134,6 @@ def main(reconstruction_parameters):
     # iterative algorithm the origin is shifted to array position [0,0,0] to avoid unnecessary fftshift calls
     measuredK = np.fft.ifftshift(measuredK)
 
-    GENFIRE.fileio.writeVolume('/Users/ajpryor/Documents/MATLAB/GENFIRE/v1.6/data/debug.mat',measuredK)
     # create a map of the spatial frequency to be used to control resolution extension/suppression behavior
     K_indices = GENFIRE.utility.generateKspaceIndices(support)
     K_indices = np.fft.fftshift(K_indices)