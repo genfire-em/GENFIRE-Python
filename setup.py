f1036b4e3e499857e5c05f3dcbb89941aab5e619
AJ Pryor <apryor6@gmail.com>
2017-06-16 10:53:36 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages,setup, Extension
 from subprocess import call
+import sys
 
 setup(
 	name 				= "genfire",
@@ -8,7 +9,8 @@ setup(
 	description 		= "GENeralized Fourier Iterative REconstruction",
 	author 				= "Alan (AJ) Pryor, Jr.",
     author_email 		= "apryor6@gmail.com",
-    install_requires	= ['Pillow>=4.1.1','numpy>=1.12.1','matplotlib>=2.0.2','scipy>=0.19.0', 'pyparsing>=2.2.0','six>=1.10.0'],#PyQt5>=5.8.2
+    #install_requires	= ['Pillow>=4.1.1','numpy>=1.12.1','matplotlib>=2.0.2','scipy>=0.19.0', 'pyparsing>=2.2.0','six>=1.10.0'] + (['PyQt5>=5.8.0'] if 'darwin' not in sys.platform else ['pyqt5-macos-built']),#PyQt5>=5.8.0
+    install_requires	= ['Pillow>=4.1.1','numpy>=1.12.1','matplotlib>=2.0.2','scipy>=0.19.0', 'pyparsing>=2.2.0','six>=1.10.0'] + (['PyQt5>=5.8.0'] if 'darwin' not in sys.platform else []),#PyQt5>=5.8.0
 	scripts				= ['bin/genfire']
 )
 
