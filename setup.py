c3cf54ec7f2ebd08775b7cf8a715da55d9f5f935
AJ Pryor <apryor6@gmail.com>
2017-10-13 18:17:50 -0400

    updating version

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import sys
 setup(
 	name 				= "genfire",
 	packages		    = find_packages(),
-	version 			= "1.0.0",
+	version 			= "1.1.5",
 	description 		= "GENeralized Fourier Iterative REconstruction",
 	author 				= "Alan (AJ) Pryor, Jr.",
     author_email 		= "apryor6@gmail.com",
-#    install_requires	= ['Pillow>=4.1.1','numpy>=1.12.1','matplotlib>=2.0.2','scipy>=0.19.0', 'pyparsing>=2.2.0','six>=1.10.0', 'PyQt5>=5.5.0'],
+    install_requires	= ['Pillow>=4.1.1','numpy>=1.12.1','matplotlib>=2.0.2','scipy>=0.19.0', 'pyparsing>=2.2.0','six>=1.10.0', 'PyQt5>=5.5.0'],
 	scripts				= ['bin/genfire']
 )
 
