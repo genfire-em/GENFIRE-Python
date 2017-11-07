facbe5fc4cf4dc2e05e3ad0f57c5109bd68b7e52
AJ Pryor <apryor6@gmail.com>
2017-11-07 10:08:27 -0500

    integration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 setup(
 	name 				= "genfire",
 	packages		    = find_packages(),
-	version 			= "1.1.7",
+	version 			= "1.1.8",
 	description 		= "GENeralized Fourier Iterative REconstruction",
 	author 				= "Alan (AJ) Pryor, Jr.",
     author_email 		= "apryor6@gmail.com",
